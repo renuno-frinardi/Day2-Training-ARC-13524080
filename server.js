8ea15b33c37c8e57b131e8b7db63025a57351495
renuno-frinardi <renunofrinardi@gmail.com>
2025-02-28 21:36:58 +0700

    feat: Added home page of the website
    Added interactive home page to the website

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const pagesPath = path.join(__dirname,'public/pages/');
+const staticPath = path.join(__dirname,'public');
+
+app.use(express.static(staticPath));
 
 app.get('/', (req, res) => {
-  res.sendFile(pagesPath + 'index.html');
+  res.sendFile(staticPath + '/pages/index.html');
 });
 
-app.listen(5005, () => {
-  console.log('Server listening on port 5005');
+app.listen(5500, () => {
+  console.log('Server listening on port 5500');
 });
